97ff0abc34532d1a6b8fa07860e3e311a1e4982d
Matej Grabovsky <mgrabovs@redhat.com>
2015-09-18 14:56:22 +0200

    wlsk test prettier printing

--- wlskout/test.py
+++ wlskout/test.py
@@ -8,26 +8,26 @@ if __name__ == '__main__':
     print('  enc_key = {}\n  mac_key = {}\b  table   = {}'.format(base.read_file('wlsk_enc_key'),
         base.read_file('wlsk_mac_key'), base.read_file('keytbl')))
 
-    print('\x1b[31mA1\x1b[0m')
+    print('\x1b[31m[A1]\x1b[0m')
     (a3, idA) = WLSK_Init.init()(base.get_hostname())
     print('  idA = {}'.format(idA))
 
-    print('\x1b[34mB2\x1b[0m')
+    print('\x1b[34m[B2]\x1b[0m')
     (b4, n) = WLSK_Resp.init()(idA)
     print('  n = {}'.format(n))
 
-    print('\x1b[31mA3\x1b[0m')
+    print('\x1b[31m[A3]\x1b[0m')
     (_, iv1, e, m) = a3(n)
     print('  e = {}\n  m = {}'.format(e, m))
 
-    print('\x1b[34mB4\x1b[0m')
+    print('\x1b[34m[B4]\x1b[0m')
     (b6, idA_, idB_, iv2, e_, m_) = b4(iv1, e, m)
     print('  idA\' = {}\n  idB\' = {}\n  e\'   = {}\n  m\'   = {}'.format(idA_, idB_, e_, m_))
 
-    print('\x1b[33mS5\x1b[0m')
+    print('\x1b[33m[S5]\x1b[0m')
     (_, iv3, e__, m__) = WLSK_S.init()(idA_, idB_, iv2, e_, m_)
     print('  e\'\' = {}\n  m\'\' = {}'.format(e__, m__))
 
-    print('\x1b[34mB6\x1b[0m')
+    print('\x1b[34m[B6]\x1b[0m')
     b6(iv3, e__, m__)
 
